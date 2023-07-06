f193ac1e3fb4f25cdcb7e5b08243bfbd9f1b1060
= <=>
2023-07-06 11:01:58 +0700

    toantu

---
+++ 7. Biến, kiểu dữ liệu và toán tử/toantu.js
@@ -0,0 +1,8 @@
+let inputWiidth = prompt('Nhập chiều Cao Hình chữ nhật:');
+let inputHeight = prompt('Nhập chiều rộng Hình chữ nhật:');
+
+let width = parseInt(inputWiidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+
+document.write('Diện tích hình chữ nhật: '+ area)
