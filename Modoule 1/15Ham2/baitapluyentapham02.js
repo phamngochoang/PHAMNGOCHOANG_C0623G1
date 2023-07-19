b8ce4eacd4f9c40523204c4204e7d3d98867dde7
= <=>
2023-07-19 16:58:51 +0700

    ham2

--- Modoule 1/15Ham2/baitapluyentapham02.js
+++ Modoule 1/15Ham2/baitapluyentapham02.js
@@ -1,5 +1,46 @@
-    //bai 1
-    function binhphuong(number){
-        return number * number;
-    }
-    console.log(binhphuong(3));
+      
+      //bai 1
+        function binhphuong(number){
+            return number * number;
+        }
+        console.log(binhphuong(3));
+
+        //bài 2:
+        // let a = Number(prompt("nhập bán kính"));
+        // function dientich(number){
+        //     return 3.14*number*number;
+        // }
+        // function chuvi(number){
+        //     return 2*3.14*number;
+        // }
+        //alert("diện tích hình tròn là:"+ dientich(a));
+        // alert("chu vi hình tròn là:"+ chuvi(a));
+
+        //bài 3:
+        // let a=Number(prompt("nhập số"));
+        // function arr(number){
+        //     let GT = 1;
+        //         for(let i=1;i<=number;i++){
+        //             GT*=i
+        //         }
+        // return GT;
+        // }alert(a + "!=" + arr(a));
+
+        //bài4:
+        // let a=prompt("nhập");
+        // function arr(value) {
+        //    return !isNaN(value);
+        // }if(arr(a)){
+        //     alert(a+"là một số");
+        // }else{alert(a+"là một kí tự");}
+
+        //bài 5:
+        // let a=prompt("nhập 3 số bất kỳ").split(",");
+        // function arr(number){
+        //     let min=number[0];
+        //     for(let i=0;i<number.length;i++){
+        //         if(number[i]<min){
+        //             min=number[i];
+        //         }
+        //     }return min;
+        // }alert("số nhỏ nhất trong 3 số là:"+arr(a));
